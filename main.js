a2820dd6bc78e206d08f55d7b2cfcd2d637060e2
LunaHale2002 <lbabic@edenschool.fr>
2024-01-12 15:13:35 +0100

    Projet perso

---
+++ main.js
@@ -0,0 +1,10 @@
+let nameIput = document.querySelector('#nameIput')
+let 
+function name(){
+    namevalue = nameIput.value
+    if( nameInput.value.lenght <3 ){
+        alert(
+            "Veuillez saisir un texte"
+        )
+    }
+}
